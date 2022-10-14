7c1bb27d7b7947ce1b3a1c2716322dca8e27d141
Luciana <lnieto@ksu.edu>
2022-10-14 16:43:47 -0500

    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,31 +5,30 @@ st.set_page_config(layout="wide")
 
 # Customize the sidebar
 markdown = """
-Web App URL: <https://template.streamlitapp.com>
-GitHub Repository: <https://github.com/giswqs/streamlit-multipage-template>
+Esta pagina es parte de los servicios de BuscoCampo, su informacion no sera compartida con terceros
+Sigannos en TW <https://twitter.com/AgRemotegirl>
 """
 
 st.sidebar.title("About")
 st.sidebar.info(markdown)
-logo = "https://i.imgur.com/UbOXYAU.png"
+logo = "https://search.brave.com/images?q=logos&img=113"
 st.sidebar.image(logo)
 
 # Customize page title
-st.title("Streamlit for Geospatial Applications")
+st.title("reportes BuscoCampo")
 
 st.markdown(
     """
-    This multipage app template demonstrates various interactive web apps created using [streamlit](https://streamlit.io) and [leafmap](https://leafmap.org). It is an open-source project and you are very welcome to contribute to the [GitHub repository](https://github.com/giswqs/streamlit-multipage-template).
+    describir o presentar algo 
     """
 )
 
-st.header("Instructions")
+st.header("Instrucciones")
 
 markdown = """
-1. For the [GitHub repository](https://github.com/giswqs/streamlit-multipage-template) or [use it as a template](https://github.com/giswqs/streamlit-multipage-template/generate) for your own project.
-2. Customize the sidebar by changing the sidebar text and logo in each Python files.
-3. Find your favorite emoji from https://emojipedia.org.
-4. Add a new app to the `pages/` directory with an emoji in the file name, e.g., `1_🚀_Chart.py`.
+1. 
+2.
+3.
 
 """
 
@@ -38,3 +37,5 @@ st.markdown(markdown)
 m = leafmap.Map(minimap_control=True)
 m.add_basemap("OpenTopoMap")
 m.to_streamlit(height=500)
+
+
